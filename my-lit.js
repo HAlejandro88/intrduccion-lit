44c35c45b7e24a79f65d35d0af3e47eaff12646c
HAlejandro88 <alexsotog82@gmail.com>
2019-02-14 12:10:42 -0600

    estilos lit

--- my-lit.js
+++ my-lit.js
@@ -19,27 +19,39 @@ class MyLit extends LitElement {
   constructor() {
     super();
     this.hello = "Hello World!";
-    this.user = "usuario";
+    this.user = "";
     this.nombre = ['alex','Ian','David','jorge','leo'];
     this.boleano = true;
   }
 
   static get styles() {
-    return css``;
+    return css`
+      :host{
+        display:block;
+        color:blue;
+      }
+      paper-input{
+        --paper-input-container-input-color:black;
+        --paper-input-container-color:black;
+        --paper-input-container-focus-color:green;
+      }
+    `;
   }
 
   render() {
     return html`
       <h2>${this.hello}</h2>
       <h1>${this.user}</h1>
-      <paper-input label="User" id="user"></paper-input>
-      <paper-input label="Password" type="password" value="${this.user}"></paper-input>
+      <paper-input label="User" id="user" @keyup="${this.evalue}"></paper-input>
+      <paper-input label="Password" type="password" id="pass"></paper-input>
       <ul>${this.nombre.map(name=>html`<li>${name}</li>`)}</ul>
       ${this.boleano?
         html`<p>Render some HTML if myBool is true</p>`:
         html`<p>Render some other HTML if myBool is false</p>`}
         <button @click="${this.changeV}">click</button>
         <button @click="${this.eval}">CHECAR</button>
+        <button @click="${this.clickHandler}">ver</button> 
+        <slot name="one"></slot>
     `;
   }
   changeV(){
@@ -47,7 +59,14 @@ class MyLit extends LitElement {
   }
 
   eval(){
-    this.shadowRoot.querySelector('#user').value;
+    let pass = this.shadowRoot.querySelector('#pass').value;
+    console.log(pass);
+  }
+  evalue(){
+    this.user = this.shadowRoot.querySelector('#user').value;
+  }
+  clickHandler(e) {
+    console.log(e.target);
   }
 }
 
